de63eb1d7b433f8bf43dd6ffae5909b36329d54b
sharath573 <35744240+sharath573@users.noreply.github.com>
2018-02-01 14:42:24 -0800

    comm

--- Exercises/exercise_01.py
+++ Exercises/exercise_01.py
@@ -11,65 +11,65 @@ By the end of class on Thursday 1/25, students should have:
     - Completed these definitions and pushed this script to a folder called "Exercises" in their repo
 
 """
-print("<----Function1---->")
 def hello():
-    print("Hello world")
     """
-    Prints "Hello World"
-    :return: None
-    """
-    return
+       Prints "Hello World"
+       :return: None
+       """
+    print("")
+
+    return hello
 
 
-hello()
 
-print("<----Function2---->")
 
 
 def percent_decimal(i):
+    """
+        Converts a percentage to a decimal or a decimal to a percentage depending on the input i
+        :param i: a float between 0 and 100
+        :return: a float between 0 and 100
+        """
 
     if i < 1:
         i = i*100
-        print("Percentage value", i, "%")
+
     elif i > 1:
         i = i/100
-        print("Decimal number", i)
-    """
-    Converts a percentage to a decimal or a decimal to a percentage depending on the input i
-    :param i: a float between 0 and 100
-    :return: a float between 0 and 100
-    """
-    return
 
 
-percent_decimal(0.5)
+    return i
 
-print("<----Function3---->")
+
+
 
 
 def exponent(integer, power):
-    print("Integer:", integer)
-    print("Power:", power)
+    """
+        Using a loop (no imports!), raise the integer given to the power provided. (integer^power)
+        :param integer: a positive, non zero, integer
+        :param power: a positive, non zero, integer
+        :return: an integer
+        """
+
     y = integer
     for x in range(power-1):
         integer = integer*y
-    """
-    Using a loop (no imports!), raise the integer given to the power provided. (integer^power)
-    :param integer: a positive, non zero, integer
-    :param power: a positive, non zero, integer
-    :return: an integer
-    """
-    print("Answer:", integer)
-    return
 
 
-exponent(2, 3)
+    return integer
 
-print("<----Function4---->")
+
+
 
 
 def complement(dna):
-    print("Given data:", dna)
+    """
+        Returns the complement strand of DNA to the input.  C <--> G,  A <--> T
+        :param dna: String containing only C, T, A, and G
+        :return: String containing only C, T, A, and G
+        """
+
     string = ''
     for char in dna:
         if char == 'C':
@@ -82,14 +82,7 @@ def complement(dna):
             string = string + 'A'
         else:
             string = string + char
-    """
-    Returns the complement strand of DNA to the input.  C <--> G,  A <--> T
-    :param dna: String containing only C, T, A, and G
-    :return: String containing only C, T, A, and G
-    """
+
     return string
 
 
-data = complement('ABCDAAGGCCTT')
-
-print("Modified data:", data)

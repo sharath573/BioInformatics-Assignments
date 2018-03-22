2948216bb0a6e1fba10293b30e953897c740b6fe
sharath573 <35744240+sharath573@users.noreply.github.com>
2018-03-22 10:00:27 -0700

    ex6

---
+++ Exercises/exercise_07.py
@@ -0,0 +1,121 @@
+"""
+exercise_07  Regular Expressions
+
+"""
+test_protein = """MHMWSFMMRFRKGGKPYHTVADARGPYDNGHLHKTYYLKNLMYYTVGMWVKEQLCCRHEP
+RSFLLAFIWSWCLAGVYRLTHRGSGPEMNRVVMFQYQDECSYLLQVTDVAYCNEQAWYAR
+FWSCIASSFEMSSHELHTQHYYTLTGSVDWNMYVQNGVWAHGIACSMTDNQDGSCRQPIM
+DTDYVDTHDHEKRYRQRGLCSTPCNDQFPGYEFPTVHGTMAWISGEMMCASGYKPYNFKH
+GEFSPKSIWEWQLFACCCCGKTEWSNVMIFMVVEIEIFASKSNEENWSGMNIIEDDDPQH
+HFKNIYRKYLDFQFYLIYGGYRMGFLHHRDSMINALVCKTGRWQQMHSFLKCDRRCKYLI
+NIYADGKWYDNWSKVEDKVLDRGIRFLQSWNATEMVNSSMYRKPFYQSQYAPRFCWETFC
+QHEIYFHQEYASNCKARCGSYQSSAQSATLAIKYSELYQGFWGTSEWGHHQLFIHRWGVQ
+IYQVKLHDSIMRPWIDWSRKCKWLLHRLHCDIELVPIHWEHKPQRVSFYYFKAMTDVLRE
+ICWSAQYHVVIQKFAFITMAWICTFYVHMYPSLDRHGSVTGIAWKLSGQWTGQEAYSKLN
+TEKETITEHNCPRVANIIVDCLEHEQVEMRMKRCHTWMVEVMKYPEQKFLWQCEHFRALY
+GLVGHNCPIKAWVRWIPHMEQKVCSYFRRAYFMMLKNPMSRDSVCRREMEYDMAISRMPY
+GDCHAVGKWLPYFLNRYTPWWVWIWMAPFQGNRFFYHYKKDWRHCGNYLGGLCVLMTFWH
+HPIYVPCPKTKADFKQACCYCGKTRQACTDENMDTQRQKFQALHQARICQPYTVRNMSIQ
+CGKNPIKANHNNNVRRPTMHICNLPAMTRRFVYTPFMKLKQSLGHDKWHIPEIQPYMDFT
+YHSVWIHQPHMNSCAACTGTVALALALALHYYTHPIQGEYIIYTSCDARYKSQKQEDIYQ
+ITSTCLEFSSKFKAQMMGTTWEIAGMNFCKVIETSCRQKI""".replace('\n', '')
+
+
+import re
+def kinase2(string):
+    """
+    Using re.findall(), return a list of matched motifs to the Casein Kinase II phosphorylation site
+    in the string.  The motif is:
+    a S or T
+    followed by any 2 Amino Acids
+    followed by a D or E
+    :param string: the protein sequence to be searched
+    :return: a list of strings
+    """
+    string = []
+    string = re.findall("[ST][A-Z][A-Z][DE]", test_protein)
+    return string
+
+#print(kinase2(test_protein))
+# #['TVAD', 'SGPE', 'SSFE', 'SSHE', 'SMTD', 'SIWE', 'SNEE', 'SKVE', 'TGQE', 'TEKE', 'TITE', 'TKAD', 'TSCD', 'TCLE', 'TTWE']
+
+
+def atp_binding_site(string):
+    """
+    Using re.findall(), return a list of matched motifs to the ATP binding motif in the string.
+    the ATP binding motif is:
+    An "A" or "G"
+    followed by any 4 Amino Acids
+    followed by a "G"
+    followed by a "K"
+    followed by a "S" or "T"
+    :param string: the protein sequence to be searched
+    :return: a list of strings
+    """
+    string=[]
+    string = re.findall("[AG][A-Z][A-Z][A-Z][A-Z][G][K][ST]", test_protein)
+    return string
+
+#print(atp_binding_site(test_protein))
+# # ['ACCCCGKT', 'ACCYCGKT']
+
+def atp_variable_region(string):
+    """
+    Using re.search() and a capture group, return the 4 variable amino acids within
+    the first matched ATP binding motif.  EX:
+    If the matched motif is ACCCCGKS, this definition should return "CCCC"
+    the ATP binding motif is:
+    An "A" or "G"
+    followed by any 4 Amino Acids
+    followed by a "G"
+    followed by a "K"
+    followed by a "S" or "T"
+    :param string: the protein sequence to be searched
+    :return: a strings
+    """
+    string = []
+    string = re.search("[AG]([A-Z]{4})[G][K][ST]", test_protein).group(1)
+    return string
+
+#print(atp_variable_region(test_protein))
+# #CCCC
+
+def atp_start_pos(string):
+    """
+    Using re.search(), find the start position of the first match to the ATP binding site motif.
+    the ATP binding motif is:
+    An "A" or "G"
+    followed by any 4 Amino Acids
+    followed by a "G"
+    followed by a "K"
+    followed by a "S" or "T"
+    :param string: the protein sequence to be searched
+    :return: int
+    """
+    return re.search("[AG][A-Z]{4}[G][K][ST]", test_protein).span()[0]
+
+#print(atp_start_pos(test_protein))
+# # 254
+
+def zinc_finger(string):
+    """
+    Using re.findall(), return a list of matched motifs to the zing finger domain
+    in the string.  The motif is:
+    a C
+    followed by 2 to 4 of any Amino Acid
+    followed by a C
+    followed by 3 of any Amino Acid
+    followed by a single L, I, V, M, F, Y, W, or C
+    followed by any 8 Amino Acids
+    followed by a single H
+    followed by 3 to 5 of any Amino Acid
+    followed by a single H
+    :param string: the protein sequence to be searched
+    :return: a list of strings
+    """
+    string = []
+    string = re.findall("[C][A-Z]{2,4}[C][A-Z]{3}[LIVMFYWC][A-Z]{8}[H][A-Z]{3,5}[H]", test_protein)
+    return string
+
+#print(zinc_finger(test_protein))
+# # ['CAACTGTVALALALALHYYTH']
